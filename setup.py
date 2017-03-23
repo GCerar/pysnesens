c78a40a5a02445083b508b2c403d2b71d4dcb808
Gregor Cerar <grega90@gmail.com>
2017-03-23 15:42:29 +0100

    Added find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='snesens',
     version='0.1',
     author='Gregor Cerar',
     author_email='gregor.cerar@ijs.si',
-    license='MIT'
+    license='MIT',
+
+    packages=find_packages('sensors')
+
 )
